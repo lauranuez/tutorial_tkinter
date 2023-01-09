9b6435c365515a7b0e225853ad6b68bb89962c74
lauranuez <lauranuezzaragoza@gmail.com>
2023-01-09 20:42:04 +0100

    Step14: markers and paths

--- MapFrameClass.py
+++ MapFrameClass.py
@@ -12,9 +12,21 @@ class MapFrameClass:
         self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga",
                                         max_zoom=22)
         self.map_widget.set_position(41.275946, 1.987475)
-        self.map_widget.set_zoom(15)
+        self.map_widget.set_zoom(20)
 
-
-
+        self.map_widget.add_right_click_menu_command(label="Insert WP", command=self.add_marker_event,pass_coords=True)
+        self.count = 0
+        self.positions = []
+        self.markers = []
 
         return self.MapFrame
+
+    def add_marker_event(self, position):
+        self.count = self.count + 1
+        marker = self.map_widget.set_marker(position[0], position[1], text=self.count)
+
+        if self.count > 1:
+            path = self.map_widget.set_path([self.positions[-1], position], color='red')
+
+        self.positions.append(position)
+        self.markers.append(marker)
